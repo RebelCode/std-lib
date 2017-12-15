61162f870d6907cbcddd842ff6d90d21d9e1b8b0
Roma Lytvynenko <litvinenko95@gmail.com>
2017-12-15 16:00:00 +0200

    Items and collection are created before every test, tests groups have short explanaition

--- test/FunctionalCollectionTest.js
+++ test/FunctionalCollectionTest.js
@@ -1,30 +1,55 @@
 import {FunctionalCollection} from './../src/FunctionalCollection';
 var assert = require('assert');
 
-var items = {
-    123: {
-        id: 123
-    },
-    125: {
-        id: 125
-    },
-    127: {
-        id: 127
-    }
-};
+/**
+ * Simple items collection.
+ *
+ * @returns {{123: {id: number}, 125: {id: number}, 127: {id: number}}}
+ */
+function createNewItems() {
+    return {
+        123: {
+            id: 123
+        },
+        125: {
+            id: 125
+        },
+        127: {
+            id: 127
+        }
+    };
+}
 
 /**
- * Testing collection sample.
+ * Create new collection based on some items.
  *
- * @type {FunctionalCollection}
+ * @returns {FunctionalCollection}
  */
-var sampleCollection = new FunctionalCollection(() => {
-    return items
-}, (newItems) => {
-    items = newItems
-}, (item) => {return item.id});
+function createNewCollection() {
+    return new FunctionalCollection(() => {
+        return items
+    }, (newItems) => {
+        items = newItems
+    }, (item) => {
+        return item.id
+    });
+}
+
+let items = {};
+let sampleCollection;
 
 describe('FunctionalCollection', function() {
+    /**
+     * This will create new items and new collection for each new test.
+     */
+    beforeEach(function(){
+        items = createNewItems();
+        sampleCollection = createNewCollection();
+    });
+
+    /**
+     * Test proper creation of collection.
+     */
     describe('#constructor', function () {
         it('should fail when not all requirements passed', function () {
             assert.throws(function() {
@@ -35,9 +60,9 @@ describe('FunctionalCollection', function() {
         it('should not fail if parameters are passed right', function () {
             assert.doesNotThrow(function() {
                 new FunctionalCollection(() => {
-                    return items;
+                    return {};
                 }, (_items) => {
-                    items = _items;
+                    // items = _items;
                 }, (item) => {
                     return item.id;
                 });
@@ -45,12 +70,18 @@ describe('FunctionalCollection', function() {
         });
     });
 
+    /**
+     * Test collection's items getter.
+     */
     describe('#getItems()', function() {
         it('check items is the same in the store object and in the collection', function() {
             assert.deepEqual(sampleCollection.getItems(), items);
         });
     });
 
+    /**
+     * Test collection's item adding.
+     */
     describe('#addItem(item)', function() {
         it('add new item to the collection', function() {
             sampleCollection.addItem({
@@ -67,6 +98,9 @@ describe('FunctionalCollection', function() {
         });
     });
 
+    /**
+     * Test collection's ability to remove items.
+     */
     describe('#removeItem(item)', function() {
         it('remove item from the collection', function() {
             sampleCollection.removeItem({
@@ -83,22 +117,9 @@ describe('FunctionalCollection', function() {
         });
     });
 
-    describe('#removeItem(item)', function() {
-        it('remove item from the collection', function() {
-            sampleCollection.removeItem({
-                id: 1
-            });
-            assert.equal(!sampleCollection.hasKey(1), true);
-        });
-
-        it('remove item from the real store', function() {
-            sampleCollection.removeItem({
-                id: 2
-            });
-            assert.equal(items[2], undefined);
-        });
-    });
-
+    /**
+     * Test collection has item checker.
+     */
     describe('#hasItem(item)', function() {
         it('check item is presented in the real store', function() {
             assert.equal(sampleCollection.hasKey(127), true);
