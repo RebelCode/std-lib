df7f002c14a64ae1c51075043a66f67b4aa5ddf7
Roma Lytvynenko <litvinenko95@gmail.com>
2018-01-09 17:10:49 +0200

    functional toggleable implementation

---
+++ src/FunctionalToggleable.js
@@ -0,0 +1,62 @@
+/**
+ * Toggleable class for encapsulate "on/off" switching
+ * logic. For example, for toggling pages visibility.
+ *
+ * @since [*next-version*]
+ */
+export class Toggleable {
+    _stateSetter;
+    _stateGetter;
+
+    constructor (set, get) {
+        this._stateSetter = set;
+        this._stateGetter = get;
+    }
+
+    /**
+     * Set On state.
+     *
+     * @since [*next-version*]
+     *
+     * @param isOn
+     * @private
+     */
+    _setState (isOn) {
+        this._stateSetter(!!isOn);
+    }
+
+    /**
+     * Get On state.
+     *
+     * @since [*next-version*]
+     *
+     * @returns {*}
+     * @private
+     */
+    _getState () {
+        return this._stateGetter();
+    }
+
+    /**
+     * Get On state.
+     *
+     * @since [*next-version*]
+     *
+     * @returns {boolean}
+     */
+    isOn () {
+        let state = this._getState();
+        return !!state;
+    }
+
+    /**
+     * Set current on state.
+     *
+     * @since [*next-version*]
+     *
+     * @param isOn
+     */
+    setState (isOn) {
+        this._setState(!!isOn);
+    }
+}
