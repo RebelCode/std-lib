160b337997043bbbdda439b43d6a9226b1c25a73
Roman Lytvynenko <litvinenko95@gmail.com>
2018-07-27 16:20:04 +0300

    version bump

--- src/DateFunctions.js
+++ src/DateFunctions.js
@@ -1,7 +1,7 @@
 /**
  * Create `createInTimezone` function.
  *
- * @param {momnent} moment Moment JS object.
+ * @param {moment} moment Moment JS object.
  *
  * @return {CreateInTimezoneFunction} `CreateInTimezone` function.
  */
@@ -19,15 +19,16 @@ export function makeCreateInTimezone (moment) {
 /**
  * Create `parseInTimezone` function.
  *
- * @param {momnent} moment Moment JS object.
+ * @param {moment} moment Moment JS object.
+ * @param {string} timezoneFreeFormat Datetime format, free from timezone information.
  *
  * @return {ParseInTimezoneFunction} `parseInTimezone` function.
  */
-export function makeParseInTimezone (moment) {
+export function makeParseInTimezone (moment, timezoneFreeFormat) {
   return (value, tz) => {
     const momentFixedTimezoneValue = moment.parseZone(value)
     if (tz.indexOf('UTC') !== 0) {
-      return moment.tz(momentFixedTimezoneValue.format(formats.tzFree), formats.tzFree, tz)
+      return moment.tz(momentFixedTimezoneValue.format(timezoneFreeFormat), timezoneFreeFormat, tz)
     }
     let offset = Number(tz.replace(/UTC\+?/g, ''))
     return momentFixedTimezoneValue.utcOffset(offset, true)
